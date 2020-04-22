e8216aa9f8d05a970a676a75183486fd28ee6ff8
GrantDakovich <gdakovich@wisc.edu>
2020-04-22 16:16:18 -0500

    added getTest endpoint

--- test_endpoint/index.js
+++ test_endpoint/index.js
@@ -6,6 +6,12 @@ const PORT = "8000";
 
 app.use(bodyParser.json());
 
+app.get("/getTest", (req,res) => {
+	res.send({
+		text: "30"
+	})
+});
+
 
 app.post("/test", (req, res) => {
 	console.log(req.body);
